38c6bea07f244295395a32195e5b4e8c29068f08
Alice M <alice.martzloff@gmail.com>
2022-01-04 22:25:29 +0100

    Make dataset_creating prettier

--- src/dataset_creating.py
+++ src/dataset_creating.py
@@ -5,4 +5,4 @@ chitchat_df = get_chitchat_df('../data/chitchat.json', 0)
 qa_df = get_qa_df('../data/qa.json', 1)
 
 # joins dataframes to csv file : our dataset
-create_dataset(chitchat_df,qa_df,'../out','dataset.csv')
+create_dataset(chitchat_df, qa_df, '../out', 'dataset.csv')
